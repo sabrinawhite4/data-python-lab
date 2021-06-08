1a37d92e760a62edcfca4e4478d09fa6677fc14e
sabrinawhite4 <sabrinawhite4@me.com>
2021-06-07 20:53:05 -0600

    initial commit

---
+++ data_presenter.py
@@ -0,0 +1,31 @@
+# PART 2
+
+# NUMBER 2
+open_file = open("CupcakeInvoices.csv")
+
+# NUMBER 3
+# for line in open_file:
+#    print(line)
+
+# NUMBER 4
+# for line in open_file:
+#    line = line.strip()
+#    values = line.split(',')
+#    print(values[2])
+
+# NUMBER 5
+# for line in open_file:
+#     line = line.strip()
+#     values = line.split(',')
+#     print(round(float(values[3]) * float(values[4])))
+
+# NUMBER 6
+# total = 0
+# for line in open_file:
+#    line = line.strip()
+#    values = line.split(',')
+#    total += float(values[3]) * float(values[4])
+# print(round(total))
+
+# NUMBER 7
+open_file.close()
